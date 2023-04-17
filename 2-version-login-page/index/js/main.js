02463cff61911e2b9a82bee3295ae606f7f36c2a
Rebeca Soares <98848463+rebecasoares982@users.noreply.github.com>
2023-04-17 14:42:42 -0300

    update

--- 2-version-login-page/index/js/main.js
+++ 2-version-login-page/index/js/main.js
@@ -5,41 +5,35 @@
 
 })(jQuery);
 
-//Surgimento do formulário de aluno e funcionário
-const btns = document.querySelectorAll('.toggle-forms .btn');
-const forms = document.querySelectorAll('.forms-container .form');
-
-btns.forEach(btn => {
-	btn.addEventListener('click', () => {
-		// Remove a classe "active" de todos os botões e formulários
-		btns.forEach(btn => btn.classList.remove('active'));
-		forms.forEach(form => form.classList.remove('active'));
-		// Adiciona a classe "active" ao botão e formulário selecionados
-		const formId = btn.getAttribute('data-form');
-		const form = document.querySelector(`#${formId}`);
-		btn.classList.add('active');
-		form.classList.add('active');
-	});
-});
-
-var estado = document.getElementById("estado");
-var municipio = document.getElementById("municipio");
-var formDiv = document.getElementById("form-div");
+const estado = document.getElementById("estado");
+const municipio = document.getElementById("municipio");
+const formDiv = document.getElementById("form-div");
+const toggleForms = document.querySelector(".toggle-forms");
+const forms = document.querySelectorAll(".form");
 
 estado.addEventListener("change", function () {
-	if (estado.value !== "" && municipio.value !== "") {
-		formDiv.style.display = "block";
-	} else {
-		formDiv.style.display = "none";
-	}
+  if (estado.value !== "" && municipio.value !== "") {
+    formDiv.style.display = "block";
+  } else {
+    formDiv.style.display = "none";
+  }
 });
 
 municipio.addEventListener("change", function () {
-	if (estado.value !== "" && municipio.value !== "") {
-		formDiv.style.display = "block";
-	} else {
-		formDiv.style.display = "none";
-	}
+  if (estado.value !== "" && municipio.value !== "") {
+    formDiv.style.display = "block";
+  } else {
+    formDiv.style.display = "none";
+  }
 });
 
-document.querySelector('.toggle-forms button:first-of-type').classList.add('active');
+toggleForms.addEventListener("change", function (event) {
+  if (event.target.type === "radio") {
+    forms.forEach(function (form) {
+      form.style.display = "none";
+    });
+
+    const selectedForm = document.getElementById(event.target.value);
+    selectedForm.style.display = "block";
+  }
+});
