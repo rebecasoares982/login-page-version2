78305bf6999e37b3291f4b98e5a6c2de1e035ffb
Rebeca Soares <98848463+rebecasoares982@users.noreply.github.com>
2023-04-17 18:25:40 -0300

    Update main.js

--- 2-version-login-page/index/js/main.js
+++ 2-version-login-page/index/js/main.js
@@ -8,15 +8,40 @@
 const formToggle = document.querySelector('.toggle-forms');
 const formAluno = document.querySelector('.form-aluno');
 const formFuncionario = document.querySelector('.form-funcionario');
+const estadoSelect = document.querySelector('#estado');
+const municipioSelect = document.querySelector('#municipio');
+
+// Esconder toggle e formulários inicialmente
+formToggle.style.display = 'none';
+formAluno.style.display = 'none';
+formFuncionario.style.display = 'none';
+
+estadoSelect.addEventListener('change', () => {
+    if (estadoSelect.value !== '' && municipioSelect.value !== '') {
+        formToggle.style.display = 'block';
+    } else {
+        formToggle.style.display = 'none';
+        formAluno.style.display = 'none';
+        formFuncionario.style.display = 'none';
+    }
+});
+
+municipioSelect.addEventListener('change', () => {
+    if (estadoSelect.value !== '' && municipioSelect.value !== '') {
+        formToggle.style.display = 'block';
+    } else {
+        formToggle.style.display = 'none';
+        formAluno.style.display = 'none';
+        formFuncionario.style.display = 'none';
+    }
+});
 
 formToggle.addEventListener('click', (event) => {
-if (event.target.value === 'form1') {
-formAluno
-
-.style.display = 'block';
-formFuncionario.style.display = 'none';
-} else if (event.target.value === 'form2') {
-formFuncionario.style.display = 'block';
-formAluno.style.display = 'none';
-}
+    if (event.target.value === 'form1') {
+        formAluno.style.display = 'block';
+        formFuncionario.style.display = 'none';
+    } else if (event.target.value === 'form2') {
+        formFuncionario.style.display = 'block';
+        formAluno.style.display = 'none';
+    }
 });
